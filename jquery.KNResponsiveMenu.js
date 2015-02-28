23533b0dfbc356201d921cf9d82d2e6b1cc97165
katsuhisanakata <katsuhisanakata@Katsuhisa-no-MacBook-Air.local>
2015-02-28 11:38:39 +0900

    α版

--- jquery.KNResponsiveMenu.js
+++ jquery.KNResponsiveMenu.js
@@ -1,13 +1,56 @@
-$(function(){
-	if ('ontouchstart' in window){
-		$('.menuButton').bind('touchstart', function(){
-			$(this).toggleClass('menuButtonAnimation');
-			$(this).next().slideToggle('fast');
-		});
-	} else {
-		$('.menuButton').click( function() {
-			$(this).toggleClass('menuButtonAnimation');
-			$(this).next().slideToggle('fast');
-		});
-	}
-});
+(function($) {
+    $.fn.KNResponsiveMenu = function(options){
+        
+        // setup options
+        var defaults = {
+            position : 'next'
+        };
+        var setting = $.extend(defaults, options);
+        
+        if ( setting.position == 'next' ) {
+	        $(this).next().hide();
+        } else if ( setting.position == 'prev' ) {
+	       $(this).prev().hide();
+        } else if ( setting.position == 'children' ) {
+	       $(this).children().hide();
+        }
+        
+        function KNToggleMenu(element, position) {
+			switch (position){
+				case 'next':
+					element.next().slideToggle('fast');
+					break;
+				case 'prev':
+					element.prev().slideToggle('fast');
+					break;
+				case 'children':
+					element.children().slideToggle('fast');
+				default:
+					break;
+			}
+		}
+
+		if ('ontouchstart' in window){
+			var touchX, touchX2 = 0;
+			var touchY, touchY2 = 0;
+			$(this).bind('touchstart', function(){
+				touchX = event.changedTouches[0].pageX;
+				touchY = event.changedTouches[0].pageY;
+			});
+			$(this).bind('touchend', function(){
+				touchX2 = event.changedTouches[0].pageX;
+				touchY2 = event.changedTouches[0].pageY;
+				if ( touchX != touchX2 || touchY != touchY2 ) {
+					return;
+				}
+				KNToggleMenu($(this), setting.position);
+			});		
+		} else {
+			$(this).click( function() {
+				KNToggleMenu($(this), setting.position);
+			});
+		}
+        
+        return(this);
+    };
+})(jQuery);
